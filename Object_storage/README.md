cc841d392a658f841a73f5977058fbfbf136790d
Đỗ Thành Đạt <dothanhdat185@gmail.com>
2024-12-05 10:10:20 +0700

    build flow

---
+++ Object_storage/README.md
@@ -0,0 +1,11 @@
+# To run minio 
+
+
+```bash
+docker-compose up -d
+```
+
+Access http://localhost:9001
+
+username: admin
+password: admin123
--- upload.py
+++ upload.py
@@ -1,11 +1,14 @@
 from minio import Minio
 from minio.error import S3Error
+import os
+
+
 
 def get_minio_client():
     client = Minio(
         "localhost:9000",
-        access_key = "minioadmin",
-        secret_key = "minioadmin",
+        access_key = os.getenv("MINIO_USERNAME"),
+        secret_key = os.getenv("MINIO_PASSWORD"),
         secure = False
     )
     return client
@@ -16,15 +19,17 @@ def upload_file(bucket_name, object_name, file_path):
 
         if not client.bucket_exists(bucket_name):
             client.make_bucket(bucket_name)
-            print(f"Bucket 'mybucket' created.")
+            print(f"Bucket '{bucket_name}' created.")
         else:
-            print(f"Bucket 'mybucket' already exists.")
+            print(f"Bucket '{bucket_name}' already exists.")
 
-
-        client.fput_object(bucket_name, object_name, file_path)
-        print(f"File 'test.csv' uploaded to bucket 'mybucket' as 'test.csv'.")
-    except S3Error as e:
-        print(f"Error uploading file: {e}")
+        if os.path.isfile(file_path):
+            client.fput_object(bucket_name, object_name, file_path)
+            print(f"File '{file_path}' uploaded to bucket '{bucket_name}' as '{object_name}'.")
+        else:
+            print(f"Error: '{file_path}' is not a file.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 # def download_file(bucket_name, object_name, file_path):
@@ -39,6 +44,6 @@ def upload_file(bucket_name, object_name, file_path):
 if __name__ == "__main__":
     bucket_name = "mybucket"
     upload_object_name = "test.csv"
-    upload_file_path = "test.csv"
+    upload_file_path = "data/test.csv"
 
     upload_file(bucket_name, upload_object_name, upload_file_path)
